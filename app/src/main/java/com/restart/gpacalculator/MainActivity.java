af9e60e4857082926dd6f8d3e53d79bb757fb6d8
restart32 <kiarashbikar@gmail.com>
2015-10-17 17:47:56 -0700

    Added some basic stuff

--- app/src/main/java/com/restart/gpacalculator/MainActivity.java
+++ app/src/main/java/com/restart/gpacalculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.restart.gpacalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner_1_L = (Spinner)findViewById(R.id.spinner_1_L);
+        ArrayAdapter<String> spinnerArrayAdapter_1_L = new ArrayAdapter<>(this, R.layout.spinner_item, getResources().getStringArray(R.array.credit_array));
+        spinnerArrayAdapter_1_L.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_1_L.setAdapter(spinnerArrayAdapter_1_L);
+
+        Spinner spinner_1_R = (Spinner)findViewById(R.id.spinner_1_R);
+        ArrayAdapter<String> spinnerArrayAdapter_1_R = new ArrayAdapter<>(this, R.layout.spinner_item, getResources().getStringArray(R.array.grade_array));
+        spinnerArrayAdapter_1_R.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_1_R.setAdapter(spinnerArrayAdapter_1_R);
     }
 }
