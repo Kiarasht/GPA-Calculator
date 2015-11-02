b4206fdc567b6822f837b1f05c5a55d620069772
restart32 <kiarashbikar@gmail.com>
2015-11-01 17:36:27 -0800

    Fixed the UI and the algorithm to calculate the GPA. Should be finished after refactoring and commeting.

--- app/src/main/java/com/restart/gpacalculator/MainActivity.java
+++ app/src/main/java/com/restart/gpacalculator/MainActivity.java
@@ -2,11 +2,14 @@ package com.restart.gpacalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends Activity {
     private Spinner spinner_1_L;
@@ -19,12 +22,102 @@ public class MainActivity extends Activity {
     private Spinner spinner_4_R;
     private Spinner spinner_5_L;
     private Spinner spinner_5_R;
+    private TextView gpa;
+    private double total;
+    private double totalattempt;
+    private double courses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupspinners();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setuplisteners();
+    }
+
+    private void calculateGPA() {
+        total = 0;
+        totalattempt = 0;
+        courses = 0;
+        if (spinner_1_L.getSelectedItemPosition() > 0) {
+            if (spinner_1_R.getSelectedItemPosition() > 0) {
+                ++courses;
+                total += (double) spinner_1_L.getSelectedItemPosition() * returnvalue(spinner_1_R.getSelectedItemPosition());
+                totalattempt += (double) spinner_1_L.getSelectedItemPosition();
+            }
+        }
+        if (spinner_2_L.getSelectedItemPosition() > 0) {
+            if (spinner_2_R.getSelectedItemPosition() > 0) {
+                ++courses;
+                total += (double) spinner_2_L.getSelectedItemPosition() * returnvalue(spinner_2_R.getSelectedItemPosition());
+                totalattempt += (double) spinner_2_L.getSelectedItemPosition();
+            }
+        }
+        if (spinner_3_L.getSelectedItemPosition() > 0) {
+            if (spinner_3_R.getSelectedItemPosition() > 0) {
+                ++courses;
+                total += (double) spinner_3_L.getSelectedItemPosition() * returnvalue(spinner_3_R.getSelectedItemPosition());
+                totalattempt += (double) spinner_3_L.getSelectedItemPosition();
+            }
+        }
+        if (spinner_4_L.getSelectedItemPosition() > 0) {
+            if (spinner_4_R.getSelectedItemPosition() > 0) {
+                ++courses;
+                total += (double) spinner_4_L.getSelectedItemPosition() * returnvalue(spinner_4_R.getSelectedItemPosition());
+                totalattempt += (double) spinner_4_L.getSelectedItemPosition();
+            }
+        }
+        if (spinner_5_L.getSelectedItemPosition() > 0) {
+            if (spinner_5_R.getSelectedItemPosition() > 0) {
+                ++courses;
+                total += (double) spinner_5_L.getSelectedItemPosition() * returnvalue(spinner_5_R.getSelectedItemPosition());
+                totalattempt += (double) spinner_5_L.getSelectedItemPosition();
+            }
+        }
+        if (courses > 0) {
+            total = total / totalattempt;
+            DecimalFormat df = new DecimalFormat("#.00");
+            gpa.setText(df.format(total));
+        }
+    }
+
+    private double returnvalue(int value) {
+        if (value == 0) {
+            Toast.makeText(getApplicationContext(), "Something went wrong.", Toast.LENGTH_SHORT).show();
+            return 0.0;
+        } else if (value == 1) {
+            return 4.0;
+        } else if (value == 2) {
+            return 3.7;
+        } else if (value == 3) {
+            return 3.33;
+        } else if (value == 4) {
+            return 3.0;
+        } else if (value == 5) {
+            return 2.7;
+        } else if (value == 6) {
+            return 2.3;
+        } else if (value == 7) {
+            return 2.0;
+        } else if (value == 8) {
+            return 1.7;
+        } else if (value == 9) {
+            return 1.3;
+        } else if (value == 10) {
+            return 1.0;
+        } else if (value == 11) {
+            return 0.7;
+        } else {
+            return 0.0;
+        }
+    }
+
+    private void setupspinners() {
         spinner_1_L = (Spinner)findViewById(R.id.spinner_1_L);
         ArrayAdapter<String> spinnerArrayAdapter_1_L = new ArrayAdapter<>(this, R.layout.spinner_item, getResources().getStringArray(R.array.credit_array));
         spinnerArrayAdapter_1_L.setDropDownViewResource(R.layout.spinner_item);
@@ -74,14 +167,15 @@ public class MainActivity extends Activity {
         ArrayAdapter<String> spinnerArrayAdapter_5_R = new ArrayAdapter<>(this, R.layout.spinner_item, getResources().getStringArray(R.array.grade_array));
         spinnerArrayAdapter_5_R.setDropDownViewResource(R.layout.spinner_item);
         spinner_5_R.setAdapter(spinnerArrayAdapter_5_R);
+
+        gpa = (TextView) findViewById(R.id.textView);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void setuplisteners() {
         spinner_1_L.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -91,6 +185,7 @@ public class MainActivity extends Activity {
         spinner_1_R.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -100,6 +195,7 @@ public class MainActivity extends Activity {
         spinner_2_L.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -109,6 +205,7 @@ public class MainActivity extends Activity {
         spinner_2_R.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -118,6 +215,7 @@ public class MainActivity extends Activity {
         spinner_3_L.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -127,6 +225,7 @@ public class MainActivity extends Activity {
         spinner_3_R.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -136,6 +235,7 @@ public class MainActivity extends Activity {
         spinner_4_L.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -145,6 +245,7 @@ public class MainActivity extends Activity {
         spinner_4_R.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -154,6 +255,7 @@ public class MainActivity extends Activity {
         spinner_5_L.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
@@ -163,6 +265,7 @@ public class MainActivity extends Activity {
         spinner_5_R.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                calculateGPA();
             }
 
             @Override
